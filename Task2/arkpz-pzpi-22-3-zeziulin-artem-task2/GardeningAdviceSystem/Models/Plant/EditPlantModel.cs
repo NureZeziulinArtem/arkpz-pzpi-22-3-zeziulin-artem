dd3b1738a90386c75bae85511e0c24cbb407c9a6
Зезюлін Артем <artem.zeziulin@nure.ua>
2024-12-28 13:30:07 +0100

    Add files via upload

---
+++ Task2/arkpz-pzpi-22-3-zeziulin-artem-task2/GardeningAdviceSystem/Models/Plant/EditPlantModel.cs
@@ -0,0 +1,13 @@
+﻿namespace GardeningAdviceSystem.Models.Plant
+{
+    public class EditPlantModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float MinPh { get; set; }
+        public float MaxPh { get; set; }
+        public int MinMoisture { get; set; }
+        public int MaxMoisture { get; set; }
+    }
+}
---
+++ Task2/arkpz-pzpi-22-3-zeziulin-artem-task2/GardeningAdviceSystem/Models/Plant/PlantModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GardeningAdviceSystem.Models.Plant
+{
+    public class PlantModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float MinPh { get; set; }
+        public float MaxPh { get; set; }
+        public int MinMoisture { get; set; }
+        public int MaxMoisture { get; set; }
+    }
+}
