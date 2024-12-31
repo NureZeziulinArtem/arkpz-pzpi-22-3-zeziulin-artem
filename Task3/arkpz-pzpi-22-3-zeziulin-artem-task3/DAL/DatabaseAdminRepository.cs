d90aa6800baf5ee811a3cd85ce22ab6018d16793
Зезюлін Артем <artem.zeziulin@nure.ua>
2024-12-31 12:33:03 +0100

    Add files via upload

--- Task3/arkpz-pzpi-22-3-zeziulin-artem-task3/DAL/DatabaseAdminRepository.cs
+++ Task3/arkpz-pzpi-22-3-zeziulin-artem-task3/DAL/DatabaseAdminRepository.cs
@@ -1,1 +1,91 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace DAL.Repositories
+{
+    public class DatabaseAdminRepository
+    {
+        private readonly ApplicationContext _context;
+        private readonly ILogger<DatabaseAdminRepository> _logger;
+
+        public DatabaseAdminRepository(ApplicationContext context, ILogger<DatabaseAdminRepository> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task BackupDatabaseAsync(string backupPath)
+        {
+            try
+            {
+                var sql = $@"
+                USE master;
+                BACKUP DATABASE [GardeningAdviceSystem]
+                TO DISK = @backupPath
+                WITH FORMAT, INIT;";
+
+                await _context.Database.ExecuteSqlRawAsync(sql, 
+                    new[] { new SqlParameter("@backupPath", backupPath) });
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to backup the database.", ex);
+            }
+        }
+
+        public async Task RestoreDatabaseAsync(string backupPath)
+        {
+            try
+            {
+                // Check if the database exists
+                var checkDatabaseExistsSql = $@"
+                USE master;
+                SELECT COUNT(*) 
+                FROM sys.databases 
+                WHERE name = 'GardeningAdviceSystem'";
+
+                var databaseExists = await _context.Database.ExecuteSqlRawAsync(checkDatabaseExistsSql) > 0;
+
+                if (databaseExists)
+                {
+                    // Disable connections to the database
+                    var disableConnectionsSql = $@"
+                    USE master;
+                    ALTER DATABASE [GardeningAdviceSystem]
+                    SET SINGLE_USER WITH ROLLBACK IMMEDIATE;";
+                    await _context.Database.ExecuteSqlRawAsync(disableConnectionsSql);
+                }
+
+                // Restore the database
+                var restoreSql = $@"
+                USE master;
+                RESTORE DATABASE [GardeningAdviceSystem]
+                FROM DISK = @backupPath
+                WITH REPLACE;";
+                await _context.Database.ExecuteSqlRawAsync(restoreSql, 
+                    new[] { new SqlParameter("@backupPath", backupPath) });
+
+                if (databaseExists)
+                {
+                    // Enable connections to the database
+                    var enableConnectionsSql = $@"
+                    USE master;
+                    ALTER DATABASE [GardeningAdviceSystem]
+                    SET MULTI_USER;";
+                    await _context.Database.ExecuteSqlRawAsync(enableConnectionsSql);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to restore the database.", ex);
+            }
+        }
+    }
+}
---
+++ Task3/arkpz-pzpi-22-3-zeziulin-artem-task3/DAL/UnitOfWork.cs
@@ -0,0 +1,148 @@
+﻿using Core.Models;
+using DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DAL
+{
+    public class UnitOfWork : IDisposable
+    {
+        private readonly ILoggerFactory _loggerFactory;
+        private readonly ILogger<UnitOfWork> _logger;
+        private readonly ApplicationContext _appContext;
+        private bool disposed = false;
+
+        private Repository<Device> _deviceRepository;
+        private Repository<Plant> _plantRepository;
+        private Repository<Fertilizer> _fertilizerRepository;
+        private Repository<CartItem> _cartItemRepository;
+        private Repository<DeviceLog> _deviceLogRepository;
+        private Repository<WateringReminder> _wateringReminderRepository;
+        private DatabaseAdminRepository _databaseAdminRepository;
+
+        public DatabaseAdminRepository DatabaseAdminRepository
+        {
+            get
+            {
+                _databaseAdminRepository ??= new DatabaseAdminRepository(_appContext,
+                        new Logger<DatabaseAdminRepository>(_loggerFactory));
+
+                return _databaseAdminRepository;
+            }
+        }
+
+        public Repository<WateringReminder> WateringReminderRepository
+        {
+            get
+            {
+                _wateringReminderRepository ??= new Repository<WateringReminder>(_appContext,
+                        new Logger<Repository<WateringReminder>>(_loggerFactory));
+
+                return _wateringReminderRepository;
+            }
+        }
+
+        public Repository<DeviceLog> DeviceLogRepository
+        {
+            get
+            {
+                _deviceLogRepository ??= new Repository<DeviceLog>(_appContext,
+                        new Logger<Repository<DeviceLog>>(_loggerFactory));
+
+                return _deviceLogRepository;
+            }
+        }
+
+        public Repository<CartItem> CartItemRepository
+        {
+            get
+            {
+                _cartItemRepository ??= new Repository<CartItem>(_appContext,
+                        new Logger<Repository<CartItem>>(_loggerFactory));
+
+                return _cartItemRepository;
+            }
+        }
+
+        public Repository<Fertilizer> FertilizerRepository
+        {
+            get
+            {
+                _fertilizerRepository ??= new Repository<Fertilizer>(_appContext,
+                        new Logger<Repository<Fertilizer>>(_loggerFactory));
+
+                return _fertilizerRepository;
+            }
+        }
+
+        public Repository<Plant> PlantRepository
+        {
+            get
+            {
+                _plantRepository ??= new Repository<Plant>(_appContext,
+                        new Logger<Repository<Plant>>(_loggerFactory));
+
+                return _plantRepository;
+            }
+        }
+
+        public Repository<Device> DeviceRepository
+        {
+            get
+            {
+                _deviceRepository ??= new Repository<Device>(_appContext,
+                        new Logger<Repository<Device>>(_loggerFactory));
+
+                return _deviceRepository;
+            }
+        }
+
+        public UnitOfWork(ApplicationContext context, ILoggerFactory loggerFactory, ILogger<UnitOfWork> logger)
+        {
+            _appContext = context;
+            _loggerFactory = loggerFactory;
+            _logger = logger;
+        }
+
+        public async Task<int> Save()
+        {
+            try
+            {
+                _logger.LogInformation("Saving changes to the database");
+
+                return await _appContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Failed to save changes to the database! Error: {errorMessage}", ex.Message);
+
+                throw new Exception($"Fail to save changes to the database: {ex.Message}");
+            }
+        }
+
+        protected virtual async Task Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    await _appContext.DisposeAsync();
+                }
+            }
+
+            this.disposed = true;
+        }
+
+        public async void Dispose()
+        {
+            await Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
