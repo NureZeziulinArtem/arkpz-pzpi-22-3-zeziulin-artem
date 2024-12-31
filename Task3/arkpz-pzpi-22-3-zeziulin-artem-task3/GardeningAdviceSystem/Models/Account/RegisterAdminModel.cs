6c5f6b82fe23b0abeae06f4dfed84790c221e5fe
Зезюлін Артем <artem.zeziulin@nure.ua>
2024-12-31 12:38:10 +0100

    Add files via upload

--- Task3/arkpz-pzpi-22-3-zeziulin-artem-task3/GardeningAdviceSystem/Models/Account/RegisterAdminModel.cs
+++ Task3/arkpz-pzpi-22-3-zeziulin-artem-task3/GardeningAdviceSystem/Models/Account/RegisterAdminModel.cs
@@ -1,1 +1,18 @@
+﻿namespace GardeningAdviceSystem.Models.Account
+{
+    public class RegisterAdminModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public AdminType AdminType { get; set; }
+    }
 
+    public enum AdminType
+    {
+        Admin,
+        DatabaseAdmin
+    }
+}
