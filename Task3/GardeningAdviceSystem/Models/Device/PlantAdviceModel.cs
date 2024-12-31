6abf2e9f322a22368c6246995e570e3abdb53704
Зезюлін Артем <artem.zeziulin@nure.ua>
2024-12-31 12:30:57 +0100

    Add files via upload

--- Task3/GardeningAdviceSystem/Models/Device/PlantAdviceModel.cs
+++ Task3/GardeningAdviceSystem/Models/Device/PlantAdviceModel.cs
@@ -1,1 +1,19 @@
+﻿namespace GardeningAdviceSystem.Models.Device
+{
+    public class PlantAdviceModel
+    {
+        public float Ph { get; set; }
+        public int Moisture { get; set; }
 
+        public float MinPh { get; set; }
+        public float MaxPh { get; set; }
+        public int MinMoisture { get; set; }
+        public int MaxMoisture { get; set; }
+
+        public float MinPhChange { get; set; }
+        public float IdealPhChange { get; set; }
+
+        public int MinMoistureChange { get; set; }
+        public int IdealMoistureChange { get; set; }
+    }
+}
